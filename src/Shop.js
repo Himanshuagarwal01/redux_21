c812f357184f1c414812dd6ca15780c7ce00a768
Himanshu agarwal <himanshu.agarwal@deeporion.com>
2023-05-11 12:28:45 +0530

    updated the website

--- src/Shop.js
+++ src/Shop.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 // import { bindActionCreator } from 'redux';
 import {  actionCreators } from './state/index';
+import shoes from "./image/shoes.jpg"
 
 const Shop = () => {
   const dispatch=useDispatch();
+  
   // const actions=bindActionCreator(actionCreators,dispatch)
   return (
-    <div>
+    <div className='Logo'>
       <h2>Buy Abibas Shop at 499 rs </h2>
+      <div>
+        <div>
+      <img src={shoes} alt="internet problem" className='shoes'></img>
+      </div>
       {/* <button onClick={(()=>{dispatch(actionCreators.withdrawMoney(499))})}className='btn btn-primary mx-2'>Remove</button>
       Add this item 
       <button onClick={(()=>dispatch(actionCreators.depositMoney(499)))}className='btn btn-primary mx-2'>Add</button> */}
-      <button onClick={(()=>{dispatch(actionCreators.withdrawMoney(499))})}className='btn btn-primary mx-2'>Remove</button>
+      <button onClick={(()=>{dispatch(actionCreators.withdrawMoney(499))})}className='btn  btn-warning mx-2'>Remove</button>
       Add this item 
-      <button onClick={(()=>dispatch(actionCreators.depositMoney(499)))}className='btn btn-primary mx-2'>Add</button>
+      <button onClick={(()=>dispatch(actionCreators.depositMoney(499)))}className='btn btn-warning mx-2'>Add</button>
+      </div>
+      
     </div>
   )
 }
